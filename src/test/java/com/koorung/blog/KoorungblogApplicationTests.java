f2296065e95041a9a47da6d2e154eeea20e57461
KoorunG <onlyzion10@naver.com>
2022-10-23 11:18:26 +0900

    project init

---
+++ src/test/java/com/koorung/blog/KoorungblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koorung.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoorungblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
