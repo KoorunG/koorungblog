79c13b3f5e68b16ea708e5edd4f306de6216856f
KoorunG <onlyzion10@naver.com>
2022-11-08 23:10:13 +0900

    add : Member 도메인 테스트 추가

--- src/test/java/com/koorung/blog/controller/MemberControllerTest.java
+++ src/test/java/com/koorung/blog/controller/MemberControllerTest.java
@@ -3,6 +3,10 @@ package com.koorung.blog.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.koorung.blog.domain.member.dto.MemberCreateDto;
+import com.koorung.blog.domain.member.entity.Address;
+import com.koorung.blog.domain.member.entity.Member;
+import com.koorung.blog.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +14,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static java.nio.charset.StandardCharsets.*;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,18 +34,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MemberControllerTest {
 
     @Autowired
+    private MemberRepository repository;
+
+    @AfterEach
+    public void tearup() {
+        repository.deleteAll();
+    }
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
     @DisplayName("회원가입 성공 테스트")
     void join_success() throws Exception {
-        mockMvc.perform(post("/members")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding(StandardCharsets.UTF_8)
-                        .content(createMember("쿠렁", "koorung", "test1234!")))
+        assertJoinMember(MemberCreateDto.builder()
+                .id("koorung")
+                .name("쿠렁")
+                .password("test1234!").build())
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -42,20 +64,106 @@ class MemberControllerTest {
     @Test
     @DisplayName("회원가입 시 비밀번호가 조건을 만족하지 않으면 예외 발생")
     void join_fail_invalid_password() throws Exception {
-        mockMvc.perform(post("/members")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding(StandardCharsets.UTF_8)
-                        .content(createMember("쿠렁", "koorung", "test34")))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("비밀번호는 특수문자, 알파벳, 숫자를 적어도 하나씩 포함해야 하며 길이는 8자 이상 12자 이하여야 합니다."))
+        assertJoinMember(MemberCreateDto.builder()
+                .id("koorung")
+                .name("쿠렁")
+                .password("test123").build())
+                // TODO) 비밀번호 체크 조건 메세지 수정_221105
+                .andExpect(jsonPath("$.message").value("3가지 조건 충족"))    // NORMAL
                 .andDo(print());
     }
 
-    private String createMember(String name, String id, String password) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(MemberCreateDto.builder()
-                .name(name)
-                .id(id)
-                .password(password)
-                .build());
+    @Test
+    @DisplayName("회원가입 시 이름을 입력하지 않으면 예외 발생")
+    void join_fail_not_exist_username() throws Exception {
+        assertJoinMember(MemberCreateDto
+                .builder()
+                .id("testId")
+                .password("test1234!@").build())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.validation.name").value("이름은 반드시 입력해야 합니다."))
+                .andDo(print());
     }
+
+    @Test
+    @DisplayName("모두 입력하지 않으면 어떻게 될까?")
+    void join_fail_not_exist_all() throws Exception {
+        //given
+        assertJoinMember(MemberCreateDto.builder().build())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.validation.password").value("비밀번호는 반드시 입력해야 합니다."))
+                .andExpect(jsonPath("$.validation.name").value("이름은 반드시 입력해야 합니다."))
+                .andExpect(jsonPath("$.validation.id").value("ID는 반드시 입력해야 합니다."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("모든 값을 입력했을 때 리턴값 체크")
+    void join_success_exist_all() throws Exception {
+        //given
+        assertJoinMember(MemberCreateDto.builder()
+                .id("koorung1234")
+                .password("test1234!@")
+                .name("쿠렁")
+                .email("koorung23@test.com")
+                .city("남양주")
+                .street("진관로")
+                .zipCode("12345")
+                .build())
+                .andExpect(status().isOk())
+                // 회원가입 시 식별자를 리턴함...
+//                .andExpect(jsonPath("$.id").value("koorung1234"))
+//                .andExpect(jsonPath("$.password").value("test1234!@"))
+//                .andExpect(jsonPath("$.email").value("koorung23@test.com"))
+//                .andExpect(jsonPath("$.role").value("USER"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("모든 회원 조회")
+    void find_all_member() throws Exception {
+        //given
+        List<Member> memberList = IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> Member.builder()
+                        .loginId("test" + i)
+                        .password("test!@#$" + i)
+                        .username("테스트" + i)
+                        .address(Address.builder()
+                                .city("남양주" + i)
+                                .zipCode("1234" + i).build())
+                        .build())
+                .collect(Collectors.toList());
+        memberRepository.saveAll(memberList);
+
+        //when
+        mockMvc.perform(get("/members")
+                        .contentType(APPLICATION_JSON)
+                        .characterEncoding(UTF_8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(5))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원의 정보 수정")
+    void patch_member_info() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    private String createMember(MemberCreateDto memberCreateDto) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(memberCreateDto);
+    }
+
+    private ResultActions assertJoinMember(MemberCreateDto memberCreateDto) throws Exception {
+        return mockMvc.perform(post("/members")
+                .contentType(APPLICATION_JSON)
+                .characterEncoding(UTF_8)
+                .content(createMember(memberCreateDto)));
+    }
+
 }
--- src/test/java/com/koorung/blog/service/MemberServiceTest.java
+++ src/test/java/com/koorung/blog/service/MemberServiceTest.java
@@ -1,19 +1,29 @@
 package com.koorung.blog.service;
 
 import com.koorung.blog.domain.member.application.MemberService;
+import com.koorung.blog.domain.member.dto.MemberUpdateDto;
+import com.koorung.blog.domain.member.entity.Address;
 import com.koorung.blog.domain.member.entity.Member;
 import com.koorung.blog.domain.member.entity.Role;
 import com.koorung.blog.domain.member.dto.MemberCreateDto;
+import com.koorung.blog.domain.member.exception.AlreadyExistMemberException;
 import com.koorung.blog.domain.member.exception.MemberNotExistException;
 import com.koorung.blog.domain.member.exception.PasswordInvalidException;
+import com.koorung.blog.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @Transactional
 @SpringBootTest
@@ -22,13 +32,16 @@ class MemberServiceTest {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private MemberRepository memberRepository;
+    private Long id;
+
     @Test
     @DisplayName("회원가입시 비밀번호가 조건에 맞지 않으면 예외 발생")
     void join_password_fail() {
         //given
         MemberCreateDto memberCreateDto = MemberCreateDto.builder()
                 .id("koorung1234")
-                .role(Role.ADMIN)
                 .password("1234")
                 .name("쿠렁")
                 .build();
@@ -40,19 +53,22 @@ class MemberServiceTest {
     @Test
     @DisplayName("관리자 회원가입 성공")
     void join_admin_success() {
+
         //given
         MemberCreateDto memberCreateDto = MemberCreateDto.builder()
                 .id("admin1234")
-                .role(Role.ADMIN)
                 .password("test1234kk!")
                 .name("관리자")
                 .build();
 
         //when
         Long memberId = memberService.join(memberCreateDto);
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistException::new);
+        member.grantAdminRole();
 
         //then
-        assertThat(memberId).isNotNull();
+        assertThat(member).isNotNull();
+        assertThat(member).extracting("role").isEqualTo(Role.ADMIN);
     }
 
     @Test
@@ -61,16 +77,21 @@ class MemberServiceTest {
         //given
         MemberCreateDto memberCreateDto = MemberCreateDto.builder()
                 .id("user1234")
-                .role(Role.USER)
                 .password("user1234@!")
                 .name("일반유저")
+                .city("남양주")
+                .street("진관로")
+                .zipCode("12345")
+                .email("koorung@naver.com")
                 .build();
 
         //when
-        Long memberId =  memberService.join(memberCreateDto);
+        Long memberId = memberService.join(memberCreateDto);
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistException::new);
 
         //then
-        assertThat(memberId).isNotNull();
+        assertThat(member).isNotNull();
+        assertThat(member).extracting("role").isEqualTo(Role.USER);
     }
 
     @Test
@@ -78,6 +99,7 @@ class MemberServiceTest {
     void none_join_guest() {
         //given
         Member member = Member.builder()
+                .role(Role.GUEST)
                 .build();
         //then
         assertThat(member).extracting("role").isEqualTo(Role.GUEST);
@@ -89,7 +111,6 @@ class MemberServiceTest {
         //given
         MemberCreateDto memberCreateDto = MemberCreateDto.builder()
                 .id("user1234")
-                .role(Role.USER)
                 .password("user1234@!")
                 .name("일반유저")
                 .build();
@@ -108,7 +129,6 @@ class MemberServiceTest {
         //givenㄹ
         MemberCreateDto memberCreateDto = MemberCreateDto.builder()
                 .id("user1234")
-                .role(Role.USER)
                 .password("user1234@!")
                 .name("일반유저")
                 .build();
@@ -123,4 +143,74 @@ class MemberServiceTest {
         assertThat(loginUser).extracting("loginId").isEqualTo("user1234");
         assertThat(loginUser).extracting("password").isEqualTo("user1234@!");
     }
+
+    @Test
+    @DisplayName("회원가입시 중복되는 로그인 아이디가 존재할 경우 예외 발생")
+    void join_fail_already_exist_loginId() {
+        //given
+        MemberCreateDto createDto = MemberCreateDto.builder()
+                .id("koorung")
+                .password("test1234!@")
+                .name("쿠렁")
+                .build();
+
+        memberService.join(createDto);
+
+        //when
+        assertThrows(AlreadyExistMemberException.class, () -> {
+            // 동일 요청으로 가입을 시도하면 에러 발생
+            memberService.join(createDto);
+        });
+    }
+
+    @Test
+    @DisplayName("모든 회원 찾기")
+    void find_all_members() {
+        //given
+        List<Member> memberList = IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> Member.builder()
+                        .loginId("test" + i)
+                        .password("test!@#$" + i)
+                        .username("테스트" + i).build())
+                .collect(Collectors.toList());
+        memberRepository.saveAll(memberList);
+
+        //when
+        List<Member> members = memberService.findAllMember();
+
+        //then
+        assertThat(members.size()).isEqualTo(5);
+        assertThat(members).extracting("username").containsExactly("테스트1", "테스트2", "테스트3", "테스트4", "테스트5");
+    }
+
+    @Test
+    @DisplayName("회원의 정보 수정 테스트")
+    void modifyMemberInfo() {
+        //given
+        List<Member> memberList = IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> Member.builder()
+                        .loginId("test" + i)
+                        .password("test!@#$" + i)
+                        .username("테스트" + i).build())
+                .collect(Collectors.toList());
+        memberRepository.saveAll(memberList);
+
+        Long id = memberList.get(0).getId();
+
+        MemberUpdateDto updateDto = MemberUpdateDto.builder()
+                .name("이름수정테스트")
+                .password("Test132!@")
+                .email("modifytest@test.com")
+                .build();
+        //when
+        Member findMember = memberRepository.findById(id).orElseThrow(MemberNotExistException::new);
+        Member updatedMember = memberService.modifyMemberInfo(findMember.getId(), updateDto);
+
+        //then
+        assertThat(updatedMember).extracting("username").isEqualTo("이름수정테스트");
+        assertThat(updatedMember).extracting("password").isEqualTo("Test132!@");
+        assertThat(updatedMember).extracting("email").isEqualTo("modifytest@test.com");
+    }
+
+    // member를 찾을때 @PathVariable을 쓸 필요가 있을까?
 }
